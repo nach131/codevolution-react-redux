cba4d7a789c508f2b86dc0128f8d6f79364a2932
nacho <nacho.mota@gmail.com>
2021-08-08 23:28:39 +0200

    5 - Actions

---
+++ INCIO/index.js
@@ -0,0 +1,8 @@
+const BUY_CAKE = 'BUY_CAKE'
+
+function buyBake() {
+  return {
+    type: BUY_CAKE,
+    info: 'First redux action'
+  }
+}
---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Button } from 'react-bootstrap';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="row">
+          <div className="col">
+
+            <Button
+              variant="outline-primary">click
+            </Button>
+
+          </div>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
